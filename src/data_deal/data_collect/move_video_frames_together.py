3315c1f4599ee523fb00212c1417df7ad6b08f27
zhourui <zhourui@streamax.com>
2021-11-04 13:58:03 +0800

    together

---
+++ src/data_deal/data_collect/move_video_frames_together.py
@@ -0,0 +1,31 @@
+import os
+import glob
+import shutil
+import tqdm
+from multiprocessing import Pool
+
+num_worker = 10
+IMG_ROOT_DIR = r'/zhourui/workspace/pro/smoke_keypoint/data/collected/fatigue_clips'
+IMG_OUT_DIR = r'/zhourui/workspace/pro/smoke_keypoint/data/smoke_keypoint_mpii/negative_images'
+if not os.path.exists(IMG_OUT_DIR):
+    os.makedirs(IMG_OUT_DIR)
+
+imgs = glob.glob(os.path.join(IMG_ROOT_DIR, '*/*.jpg'))
+print("Total {} images".format(len(imgs)))
+
+def deal(item):
+    img, idx = item
+
+    _, dname, imgname = img.rsplit('/', maxsplit=2)
+    out_name = dname+'_'+imgname
+    out_path = os.path.join(IMG_OUT_DIR, out_name)
+
+    shutil.copy(img, out_path)
+
+
+with Pool(num_worker) as pool:
+    r = list(tqdm.tqdm(pool.imap(
+        deal,
+        zip(imgs, range(len(imgs))))))
+
+
