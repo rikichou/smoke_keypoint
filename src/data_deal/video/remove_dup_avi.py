11ad362acd08cdcf9c23c82e948f1bea23e653ff
ruiming <zhourui@streamax.com>
2021-12-17 09:57:09 +0800

    remove dup

---
+++ src/data_deal/video/remove_dup_avi.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import shutil
+
+VIDEO_ROOT_DIR = r'/home/ruiming/workspace/pro/smoke_keypoints/smoke_keypoint/data/collect'
+
+videos = os.listdir(VIDEO_ROOT_DIR)
+duplist = []
+
+for v in videos:
+    if '.avi' not in v:
+        continue
+    basename = os.path.splitext(v)[0]
+    checkname = basename+'.264.avi'
+
+    if os.path.exists(os.path.join(VIDEO_ROOT_DIR, checkname)):
+        duplist.append(checkname)
+
+print(duplist)
+print(len(duplist))
